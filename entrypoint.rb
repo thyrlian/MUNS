b5d6d5847000a91dcb3f2bd01beaefe014e58cd2
Jing Li <jingli@ebay.com>
2016-05-12 05:57:26 +0200

    Objectize entrypoint ruby script

--- entrypoint.rb
+++ entrypoint.rb
@@ -1,40 +1,59 @@
 #!/usr/bin/env ruby
 
-def salute
-  puts '=' * 50
-  puts ' ███╗   ███╗ ██╗     ██╗ ███╗    ██╗ ██████████╗'
-  puts ' ████╗ ████║ ██║     ██║ ████╗   ██║ ██╔═══════╝'
-  puts ' ██╔████╔██║ ██║     ██║ ██╔██╗  ██║ ██████████╗'
-  puts ' ██║╚██╔╝██║ ██║     ██║ ██║ ╚██╗██║ ╚═══════██║'
-  puts ' ██║ ╚═╝ ██║ ╚████████╔╝ ██║   ╚███║ ██████████║'
-  puts ' ╚═╝     ╚═╝  ╚═══════╝  ╚═╝    ╚══╝ ╚═════════╝'
-  puts ''
-  puts " #{colorize('MongoDB', :green)} + #{colorize('Unicorn', :magenta)} + #{colorize('Nginx', :green)} + #{colorize('Sinatra', :red)}"
-  puts '=' * 50
-end
-
-def colorize(text, color)
-  case color
-  when :black
-    color_code = 30
-  when :red
-    color_code = 31
-  when :green
-    color_code = 32
-  when :yellow
-    color_code = 33
-  when :blue
-    color_code = 34
-  when :magenta
-    color_code = 35
-  when :cyan
-    color_code = 36
-  when :white
-    color_code = 37
-  else
-    return text
+module MUNS
+  class Entrypoint
+    def salute
+      puts '=' * 50
+      puts ' ███╗   ███╗ ██╗     ██╗ ███╗    ██╗ ██████████╗'
+      puts ' ████╗ ████║ ██║     ██║ ████╗   ██║ ██╔═══════╝'
+      puts ' ██╔████╔██║ ██║     ██║ ██╔██╗  ██║ ██████████╗'
+      puts ' ██║╚██╔╝██║ ██║     ██║ ██║ ╚██╗██║ ╚═══════██║'
+      puts ' ██║ ╚═╝ ██║ ╚████████╔╝ ██║   ╚███║ ██████████║'
+      puts ' ╚═╝     ╚═╝  ╚═══════╝  ╚═╝    ╚══╝ ╚═════════╝'
+      puts ''
+      puts " #{colorize('MongoDB', :green)} + #{colorize('Unicorn', :magenta)} + #{colorize('Nginx', :green)} + #{colorize('Sinatra', :red)}"
+      puts '=' * 50
+    end
+    
+    def print_version
+      system('ruby --version')
+      system('mongo --version')
+      system('unicorn -v')
+      system('nginx -v')
+      system('gem list sinatra | grep "^sinatra\s.*"')
+    end
+    
+    def run_daemon
+      system('while true; do sleep 3600; done')
+    end
+    
+    def colorize(text, color)
+      case color
+      when :black
+        color_code = 30
+      when :red
+        color_code = 31
+      when :green
+        color_code = 32
+      when :yellow
+        color_code = 33
+      when :blue
+        color_code = 34
+      when :magenta
+        color_code = 35
+      when :cyan
+        color_code = 36
+      when :white
+        color_code = 37
+      else
+        return text
+      end
+      "\e[#{color_code}m#{text}\e[0m"
+    end
+    
+    private :colorize
   end
-  "\e[#{color_code}m#{text}\e[0m"
 end
 
-salute
+entrypoint = MUNS::Entrypoint.new
+entrypoint.salute
