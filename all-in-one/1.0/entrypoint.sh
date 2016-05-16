1c4e67e17eff2b6e9e81e2de847d7c06695542b6
Jing Li <jingli@ebay.com>
2016-05-16 21:04:29 +0200

    Add entrypoint shell script

---
+++ all-in-one/1.0/entrypoint.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -eo pipefail
+
+function salute() {
+  echo '=================================================='
+  echo ' ███╗   ███╗ ██╗     ██╗ ███╗    ██╗ ██████████╗'
+  echo ' ████╗ ████║ ██║     ██║ ████╗   ██║ ██╔═══════╝'
+  echo ' ██╔████╔██║ ██║     ██║ ██╔██╗  ██║ ██████████╗'
+  echo ' ██║╚██╔╝██║ ██║     ██║ ██║ ╚██╗██║ ╚═══════██║'
+  echo ' ██║ ╚═╝ ██║ ╚████████╔╝ ██║   ╚███║ ██████████║'
+  echo ' ╚═╝     ╚═╝  ╚═══════╝  ╚═╝    ╚══╝ ╚═════════╝'
+  printf ' '
+  color_print 'MongoDB' 'green'
+  printf ' + '
+  color_print 'Unicorn' 'magenta'
+  printf ' + '
+  color_print 'Nginx' 'green'
+  printf ' + '
+  color_print 'Sinatra' 'red'
+  echo ''
+  echo '=================================================='
+}
+
+function list_versions_info() {
+  echo ''
+  ruby --version
+  mongo --version
+  unicorn -v
+  nginx -v
+  gem list sinatra | grep "^sinatra\s.*"
+  echo ''
+}
+
+function run_nop_daemon() {
+  while true
+  do
+    sleep 3600
+  done
+}
+
+function color_print() {
+  if [[ $# -lt 1 ]]; then
+    exit 1
+  fi
+  local text=$1
+  local color=$2
+  case "$color" in
+    "black") printf "\e[30m$text\e[0m"
+    ;;
+    "red") printf "\e[31m$text\e[0m"
+    ;;
+    "green") printf "\e[32m$text\e[0m"
+    ;;
+    "yellow") printf "\e[33m$text\e[0m"
+    ;;
+    "blue") printf "\e[34m$text\e[0m"
+    ;;
+    "magenta") printf "\e[35m$text\e[0m"
+    ;;
+    "cyan") printf "\e[36m$text\e[0m"
+    ;;
+    "white") printf "\e[37m$text\e[0m"
+    ;;
+    *) printf "\e[0m$text"
+    ;;
+  esac
+}
